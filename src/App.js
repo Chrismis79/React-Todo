8f1ea253c18f5e354e4d56a229360df173acd537
Chrismis79 <chrismis79@gmail.com>
2019-10-28 14:58:38 -0500

    app class and constructor built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+const taskData = [
+  {
+    task: "Build React Todo App",
+    id: 10282019,
+    completed: true
+  }
+]
+ 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      items: taskData
+    };
+  }
+
+  addItem = itemName => {
+    const newItem = {
+      task: itemName,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      items: [...this.state.items, newItem]
+    });
+  };
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Christine's Todo App!</h2>
+        <TodoForm addItem={this.addItem}/>
       </div>
     );
   }
