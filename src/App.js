a95bb708e9e264bb86f578faeeb642604f1f8de1
Chrismis79 <chrismis79@gmail.com>
2019-10-28 14:29:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -14,3 +14,17 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+// [
+//   {
+//     task: 'Organize Garage',
+//     id: 1528817077286,
+//     completed: false
+//   },
+//   {
+//     task: 'Bake Cookies',
+//     id: 1528817084358,
+//     completed: false
+//   }
+// ];
